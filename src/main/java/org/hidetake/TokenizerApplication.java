c7cd3cb7db9a38bf4cb510efa029a66e0ead4264
int128 <int128@gmail.com>
2016-06-09 16:13:51 +0900

    Initial import

---
+++ src/main/java/org/hidetake/TokenizerApplication.java
@@ -0,0 +1,12 @@
+package org.hidetake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenizerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/hidetake/TokenizerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.hidetake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TokenizerApplication.class)
+public class TokenizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
